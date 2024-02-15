794382b11f9dac7d2916beabbb42945e98f16db7
Sourav Kumar <144293972+s0uravk@users.noreply.github.com>
2024-02-14 22:13:47 -0500

    Optimized code with creating new functions

--- static/js/app.js
+++ static/js/app.js
@@ -1,174 +1,129 @@
-// Get the Data from samples.json
+// Constants
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
-// Update the restyled plot's values
-function optionChanged(type, newdata) {
-    Plotly.react(type, newdata);
-}
-
-// Function for data in Demographic info Panel
-function panel(response, dataset){
+// Function for displaying demographic info panel
+function panel(response, dataset) {
+    // Fetching data from the provided JSON URL
     d3.json(url).then(response => console.log(response));
-let demoArrKey = Object.keys(response.metadata.find(data => data.id == dataset));
-        let demoArrVal = Object.values(response.metadata.find(data => data.id == dataset));        
-        for (let i = 0; i< demoArrKey.length; i++){
-            d3.select('#sample-metadata').append('div').text(demoArrKey[i] + ' : ' + demoArrVal[i])
-            }
+    
+    // Extracting keys and values from metadata for the selected dataset
+    let demoArrKey = Object.keys(response.metadata.find(data => data.id == dataset));
+    let demoArrVal = Object.values(response.metadata.find(data => data.id == dataset));        
+    
+    // Displaying demographic information in the panel
+    for (let i = 0; i < demoArrKey.length; i++) {
+        d3.select('#sample-metadata').append('div').text(`${demoArrKey[i]} : ${demoArrVal[i]}`)
+    }
 }
 
-// Adding Options to Select tag with value attribute
-d3.json(url).then(response =>{
+// Function to update the restyled plot's values
+function optionChanged(type, newdata) {
+    // Clearing existing content of the specified HTML element
+    d3.select(type).html('');
+    // Creating a new Plotly plot with updated data
+    Plotly.newPlot(type, newdata);
+}
 
-    const names = Object.values(response.names);
-    for(let i = 0; i < names.length;i++){
-    d3.select('#selDataset').append('option').text(names[i]).attr('value', names[i]);
-}})
+// Function to fetch data and initialize default visualization
+function init() {
+    // Fetching data
+    d3.json(url).then(response => {       
+        // Plotting default visualization
+        plot(response, 940);
+        // Displaying demographic info panel for default dataset
+        panel(response, 940);        
+    });
+}
 
-// Display default visualization
-function init(){
-    // Fetch data
+// Function to handle data fetching and visualization update on dropdown change
+function getData() {
+    // Fetching data
     d3.json(url).then(response => {
-       
-        // Extract required data from the response
-        let sampleValues = response.samples[0].sample_values.sort((a, b) =>b-a).slice(0,10).reverse();
-        let otuIds = response.samples[0].otu_ids.slice(0,10).reverse();
-        let otuLabels = response.samples[0].otu_labels.slice(0,10).reverse();
-        console.log(sampleValues);
-        console.log(otuIds);
-        console.log(otuLabels);
-        // Create data object for Plotly Bar Chart
-        let dataBar = [{
-            x: sampleValues,
-            y: otuIds.map(id => `OTU ${id}`),
-            text: otuLabels,
-            type: 'bar',
-            orientation: 'h'
-        }];
+        // Getting the selected dataset from the dropdown menu
+        let selection = d3.select('#selDataset');
+        let dataset = selection.property('value');
+        console.log(dataset);
+        // Updating the plots and demographic info panel with the selected dataset
+        plot(response, dataset);        
+        d3.select('#sample-metadata').html('');
+        panel(response, dataset);
+        console.log(Object.values(response.metadata.find(data => data.id == dataset)));
+    });
+}
 
-        let layoutBar = {
-            width: 400,
-            height: 600
-        }
+// Function to plot data
+function plot(response, dataset) {
+    // Extracting required data from the response
+    let sampleData = response.samples.filter(data => data.id == dataset)[0];
+    let sampleValues = sampleData.sample_values.sort((a, b) => b - a).slice(0, 10).reverse();
+    let otuIds = sampleData.otu_ids.slice(0,10).reverse();
+    let otuLabels = sampleData.otu_labels.slice(0,10).reverse();
+    console.log(sampleValues);
+    console.log(otuIds);
+    console.log(otuLabels);
 
-        // Create plot using Plotly
-        Plotly.newPlot('bar', dataBar, layoutBar);
-
-        let sampleValues1 = response.samples[0].sample_values.sort((a, b) =>b-a).reverse();
-        let otuIds1 = response.samples[0].otu_ids.reverse();
-        let otuLabels1 = response.samples[0].otu_labels.reverse();
-
-        // Create data object for Plotly Bubble Chart
-        let dataBubble =[{
-            x : otuIds1,
-            y: sampleValues1,
-            text : otuLabels1,
-            mode: 'markers',
-            marker: {
-                size: sampleValues1,
-                color : otuIds1,
-                colorscale: 'Viridis',
-            }
-
-        }]
-
-        let layoutBubble= {           
-            height: 550,
-            width: 1500,
-            xaxis: {
-                title: 'OTU ID'
-            }
-          };
-          // Create plot using Plotly
-          Plotly.newPlot('bubble', dataBubble, layoutBubble);
-        
-        panel(response, 940)
-        console.log(Object.values(response.metadata[0]))
+    // Creating data object for Plotly Bar Chart
+    let dataBar = [{
+        x: sampleValues,
+        y: otuIds.map(id => `OTU ${id}`),
+        text: otuLabels,
+        type: 'bar',
+        orientation: 'h'
+    }];
     
-        let wfreq = response.metadata.filter(data => data.id == 940)[0].wfreq
+    // Updating the bar chart
+    optionChanged('bar', dataBar);
+
+    let sampleValues1 = sampleData.sample_values.sort((a, b) =>b-a).reverse();
+    let otuIds1 = sampleData.otu_ids.reverse();
+    let otuLabels1 = sampleData.otu_labels.reverse();
+
+    // Creating data object for Plotly Bubble Chart
+    let dataBubble =[{
+        x : otuIds1,
+        y: sampleValues1,
+        text : otuLabels1,
+        mode: 'markers',
+        marker: {
+            size: sampleValues1,
+            color : otuIds1,
+            colorscale: 'Viridis',
+        }
         
-        let gaugeChart = [{
-            value: wfreq,
-            title: {text: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week"},
-            type: "indicator",
-            mode: "gauge+number+delta",
-            gauge: {
-                axis: { range: [null, 9] }},
-            threshold: {
-                    line: { color: "red", width: 4 },
-                    thickness: 0.75}
-            
-    }]
-        // Plot the gauge chart
-        Plotly.newPlot('gauge', gaugeChart);
-})}
-// On change to the DOM, call getData()
+    }];
+
+    // Updating the bubble chart
+    optionChanged('bubble', dataBubble);
+    
+    let wfreq = response.metadata.filter(data => data.id == dataset)[0].wfreq;
+
+    let gaugeChart = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: wfreq,
+        title: {text: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week"},
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+            axis: { range: [null, 9] }
+        }
+    }];
+
+    // Updating the gauge chart
+    optionChanged('gauge', gaugeChart);
+}
+
+// Event listener for dropdown change
 d3.selectAll("#selDataset").on("change", getData);
 
-function getData(){
-    // Fetch data
-    d3.json(url).then(response => {
-        let selection = d3.select('#selDataset');
-        // Assign the value of the dropdown menu option to a letiable
-        let dataset = selection.property('value');
-        console.log(dataset)
-         
-        // Extract required data from the response
-        let sampleData = response.samples.filter(data => data.id == dataset)[0];
-        let sampleValues = sampleData.sample_values.sort((a, b) => b - a).slice(0, 10).reverse();
-        let otuIds = sampleData.otu_ids.slice(0,10).reverse();
-        let otuLabels = sampleData.otu_labels.slice(0,10).reverse();
-        console.log(sampleValues);
-        console.log(otuIds);
-        console.log(otuLabels);
-        // Create data object for Plotly Bar Chart
-        let dataBar = [{
-            x: sampleValues,
-            y: otuIds.map(id => `OTU ${id}`),
-            text: otuLabels,
-            type: 'bar',
-            orientation: 'h'
-        }];
-        optionChanged('bar', dataBar);
+// Adding Options to Select tag with value attribute
+d3.json(url).then(response => {
+    // Extracting names from the response
+    const names = Object.values(response.names);
+    // Adding options to the dropdown menu
+    for (let i = 0; i < names.length; i++) {
+        d3.select('#selDataset').append('option').text(names[i]).attr('value', names[i]);
+    }
+});
 
-        let sampleValues1 = sampleData.sample_values.sort((a, b) =>b-a).reverse();
-        let otuIds1 = sampleData.otu_ids.reverse();
-        let otuLabels1 = sampleData.otu_labels.reverse();
-
-        // Create data object for Plotly Bubble Chart
-        let dataBubble =[{
-            x : otuIds1,
-            y: sampleValues1,
-            text : otuLabels1,
-            mode: 'markers',
-            marker: {
-                size: sampleValues1,
-                color : otuIds1,
-                colorscale: 'Viridis',
-            }
-            
-        }]
-
-        optionChanged('bubble', dataBubble);
-        
-        let wfreq = response.metadata.filter(data => data.id == dataset)[0].wfreq;
-
-        let gaugeChart = [{
-            domain: { x: [0, 1], y: [0, 1] },
-            value: wfreq,
-            title: {text: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week"},
-            type: "indicator",
-            mode: "gauge+number",
-            gauge: {
-                axis: { range: [null, 9] }}
-            
-    }]
-        optionChanged('gauge', gaugeChart)
-               
-        d3.select('#sample-metadata').html('')
-        panel(response, dataset)
-        console.log(Object.values(response.metadata.find(data => data.id == dataset)))
-})}
-
-
-
-  init();
+// Initialize the visualization
+init();
